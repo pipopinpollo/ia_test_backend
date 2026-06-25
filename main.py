85dd1f743b09738e233757faa90fd44d09abcaaa
Pedro Herrero <pedro.herrero@siigo.com>
2026-06-25 17:37:50 -0300

    Habilitar CORS para red privada local

--- main.py
+++ main.py
@@ -25,16 +25,8 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_private_network=True,
 )
-
-
-@app.middleware("http")
-async def allow_private_network_access(request, call_next):
-    response = await call_next(request)
-    if request.headers.get("access-control-request-private-network") == "true":
-        response.headers["Access-Control-Allow-Private-Network"] = "true"
-    return response
-
 
 app.include_router(users_router)
 app.include_router(teams_router)
